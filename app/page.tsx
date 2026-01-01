7f0c8006bd0bbe29f02881897c6b6ec6de9880b4
Jokoyoski <jookoyoski@gmail.com>
2026-01-01 01:15:50 +0100

    fix: ensured that the vendor app works properly

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,8 @@ import { useFetchAnalytics } from "./_lib/dashboard";
 interface ProductType {
   _id: string;
   name: string;
-  discounted_price: number;
+  cost_price: number;
+  discounted_price: number | null;
   status: string;
   quantity: number;
   images: string[];
